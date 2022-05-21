a234a31ce347e1c4d05359223ad8f489f784bd34
shubhamtikka <t.shubham1910@gmail.com>
2022-05-22 01:14:58 +0530

    First commit

---
+++ src/main/java/com/PersonalCRM/PersonalCRMRestAPI/PersonalCrmRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.PersonalCRM.PersonalCRMRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCrmRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCrmRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PersonalCRM/PersonalCRMRestAPI/PersonalCrmRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PersonalCRM.PersonalCRMRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCrmRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
